4654b0d31517ee77b1313642df73b1bedfc7c79d
NBaille <36842872+MSGOD@users.noreply.github.com>
2023-02-08 19:00:21 -0500

    test

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const { useStore } = require('vuex')
+const app = express()
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const path = require('path')
+
+const db = mysql.createPool({
+    host: 'us-cdbr-east-06.cleardb.net',
+    user: 'b55b54d996426f',
+    password: 'c3cee916',
+    database: 'heroku_88940b006fcfea1'
+})
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.join(__dirname + "/public")))
+app.use(bodyParser.urlencoded({extended: true}))
+const PORT = process.env.PORT || 3002
+
+app.post('/api/insert', (req, res) => {
+
+    const InputGrade = req.body.InputGrade
+    const InputName = req.body.InputName 
+    const InputComment = req.body.InputComment
+
+    const sqlInsert = 
+        "INSERT INTO ratings (create_time, grade, name, commentaire) VALUES (NOW(), ?, ?, ?);"
+    db.query(sqlInsert, [InputGrade, InputName, InputComment], (err, result) => {
+        console.log(result)
+    })
+})
+
+app.listen(PORT)
