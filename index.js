86e1270a9eefd12998b9d381a3d464f631ef1d7c
NBaille <36842872+MSGOD@users.noreply.github.com>
2023-02-08 20:11:49 -0500

    fsdv

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static(path.join(__dirname + "/public")))
 app.use(bodyParser.urlencoded({extended: true}))
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 
 app.post('/api/insert', (req, res) => {
 
